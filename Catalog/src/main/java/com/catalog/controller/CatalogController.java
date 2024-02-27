f3d52553ac8d615f18070630c6e33e9cfd981f91
USER <USER@DESKTOP-2EQ86HI>
2024-02-27 16:20:16 +0900

    MappingJackson2JsonView

--- Catalog/src/main/java/com/catalog/controller/CatalogController.java
+++ Catalog/src/main/java/com/catalog/controller/CatalogController.java
@@ -3,9 +3,8 @@ package com.catalog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,23 +16,28 @@ public class CatalogController {
  
 	@Autowired
 	CatalogService catalogService;
-
-	@RequestMapping("/hello")
+	
+	@ResponseBody
+	@RequestMapping("/hello.do")
 	public ModelAndView getListBoard() {
+		System.out.println("123123");
 		CatalogVO param = new CatalogVO();
-		ModelAndView mav = new ModelAndView();
+		ModelAndView mav = new ModelAndView("jsonView");
 		mav.addObject("model", catalogService.testQuery(param));
 		return mav;
 	}
 	
+	
 	@RequestMapping("/testPage")
-	public ModelAndView testPage(ModelAndView mav) {
+	public ModelAndView testPage() {
+		ModelAndView mav = new ModelAndView("test");
+		
 		CatalogVO param = new CatalogVO();
-		mav.setViewName("test");		
+		
 		List<CatalogVO> testModel = catalogService.testQuery(param);
-		mav.addObject("model",testModel);
+		
 		System.out.println(testModel);
-		System.out.println(mav.getViewName());
+		
 		return mav;
 	}
 
